890427e899aea87bbc3cbee43f224050a1b5da5c
Romek. <romek.solodkov@tktk.ee>
2024-02-29 16:25:36 +0200

    add myfirstapp

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter youre name");
+string username;
+username = Console.ReadLine();
+Console.WriteLine("Hello" + "," + username +"!");
+Console.WriteLine($"Hello, {username}!");
